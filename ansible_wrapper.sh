0dfb42f597cf049bae3988330af48aa8a6eebcf1
Nikita K <nikwhite333@gmail.com>
2023-01-03 02:25:02 +0400

    Added more test cases for ansible

--- ansible_wrapper.sh
+++ ansible_wrapper.sh
@@ -1,2 +1,6 @@
 echo Hello, World!
+MANAGED_NODE=Linux3
 ansible Linux3 -m ping -i inventory
+ansible $MANAGED_NODE -m package -a "name=bash-completion state=present" -i inventory -b -K
+ansible $MANAGED_NODE -a "uptime" -i inventory
+ansible all -m ansible.builtin.setup -i inventory
